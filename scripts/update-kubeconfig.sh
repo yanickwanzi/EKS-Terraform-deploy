03ab14e6a28d11da8a1e32f06e6cc0b7df5362dd
Ndiforamang Fusi <fusisoft@gmail.com>
2024-06-27 13:15:37 -0500

    Added terraform code to deploy aws resources

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-aws eks list-clusters --region us-west-2 --output text 2>&1
-if  [ $? != 0 ]
-then
-    echo "dominion-cluster does not exist"
+if aws eks list-clusters --region us-west-2 --output text 2>&1 ; then
 else
-    echo "updating cluster kubeconfig file "
+  e=$?        # return code from if
+  if [ "${e}" != 0 ]; then
+    echo "dominion-cluster does not exist"
+  elif [ "${e}" == 0]; then
+    echo "updating cluster kubeconfig file"
     aws eks --region us-west-2 update-kubeconfig --name dominion-cluster && export KUBE_CONFIG_PATH=~/.kube/conf
+  fi
 fi
