41d3fb0162d469b4ee1c234d10bd16115185112a
Ndiforamang Fusi <fusisoft@gmail.com>
2024-06-27 08:22:52 -0500

    Added terraform code to deploy aws resources

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -46,12 +46,12 @@ module "eks" {
     node-group-01 = {
       min_size     = 1
       max_size     = 10
-      desired_size = 4
+      desired_size = 2
     }
     node-group-02 = {
       min_size     = 1
       max_size     = 10
-      desired_size = 5
+      desired_size = 2
 
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
