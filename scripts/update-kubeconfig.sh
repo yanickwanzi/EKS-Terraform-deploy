f61098886fafcff4f7309cd1ad3943e152c1a54f
Ndiforamang Fusi <fusisoft@gmail.com>
2024-06-29 12:44:21 -0500

    modified bash script

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -2,6 +2,7 @@
 response="$(aws eks list-clusters --region us-west-2 --output text | grep -i dominion-cluster 2>&1)" 
 if [[ $? -eq 0 ]]; then
     echo "Success: Dominion-cluster exist"
+    aws eks --region us-west-2 update-kubeconfig --name dominion-cluster && export KUBE_CONFIG_PATH=~/.kube/config
 else
     echo "Error: Dominion-cluster does not exist"
 fi
