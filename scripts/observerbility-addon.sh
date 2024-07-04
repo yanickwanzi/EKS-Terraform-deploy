2087de2fe71b79d0e1b6c27bbc5afbe611c65b3b
Ndiforamang Fusi <fusisoft@gmail.com>
2024-07-04 05:18:40 -0500

    modified bash script

--- scripts/observerbility-addon.sh
+++ scripts/observerbility-addon.sh
@@ -3,7 +3,13 @@ response="$(aws eks list-clusters --region us-west-2 --output text | grep -i dom
 if [[ $? -eq 0 ]]; then
     echo "Deploying observerbility addon in dominion cluster"
     aws iam attach-role-policy \
+    --role-name node-group-02-eks-node-group-20240704053204609000000001 \
     --policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy 
+
+     aws iam attach-role-policy \
+    --role-name node-group-01-eks-node-group-20240704053204610200000002 \
+    --policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
+
     aws eks create-addon --cluster-name dominion-cluster --addon-name amazon-cloudwatch-observability
 
 else
