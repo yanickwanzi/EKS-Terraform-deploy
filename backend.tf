3f2e3ccf0dd27b5f20d94183f966b12cec009bfc
yanickwanzi <wanzijs24@gmail.com>
2024-07-11 12:00:13 -0500

    bucket name adjusted

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "terraformstatefile"
-    bucket         = "yanick-bucket-"
+    bucket         = "yanick-bucket-v5"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
