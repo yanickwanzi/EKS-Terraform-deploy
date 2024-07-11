7cf1a057e1a70103c7edc008de34a8ffde91e76d
yanickwanzi <wanzijs24@gmail.com>
2024-07-11 11:40:54 -0500

    userdata modified

--- modules/terraform_node/userdata.sh
+++ modules/terraform_node/userdata.sh
@@ -27,6 +27,6 @@ sudo apt update -y
 sudo apt-get install terraform -y
 
 #Installing kubectl client
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.12/2024-04-19/bin/linux/amd64/kubectl
+curl -O https://s3.us-east-2.amazonaws.com/amazon-eks/1.27.12/2024-04-19/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
