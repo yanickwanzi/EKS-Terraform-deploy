c7400e7e8d89e4786d83a720b1e5cfc8aad745db
yanickwanzi <wanzijs24@gmail.com>
2024-07-11 11:35:41 -0500

    terraform backed setup

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "infra/terraformstatefile"
-    bucket         = "fusisoft-terraform-backend-bucket"
-    region         = "us-west-2"
+    key            = "terraformstatefile"
+    bucket         = "yanick-bucket-"
+    region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
 }
