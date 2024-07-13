56fe477b0791a56f4a11e6ef8ac972e3fe3e3c2f
yanickwanzi <wanzijs24@gmail.com>
2024-07-13 18:29:43 -0500

    state file modified

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "terraformstatefile"
+    key            = "infra/terraformstatefile"
     bucket         = "yanick-bucket-v5"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
